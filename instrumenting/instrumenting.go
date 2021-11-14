ca0dfd699fd3ee7262fa13a24b15adc29210b48d
aufawibowo <muhammad.aufa@fazzfinancial.com>
2021-11-14 16:42:38 +0700

    add instrumenting pkg

---
+++ instrumenting/instrumenting.go
@@ -0,0 +1,38 @@
+package instrumenting
+
+import (
+	"fmt"
+	"github.com/aufawibowo/go-kit/service"
+	"github.com/go-kit/kit/metrics"
+	"time"
+)
+
+type Middleware struct {
+	requestCount   metrics.Counter
+	requestLatency metrics.Histogram
+	countResult    metrics.Histogram
+	next           service.StringService
+}
+
+func (mw Middleware) Uppercase(s string) (output string, err error) {
+	defer func(begin time.Time) {
+		lvs := []string{"method", "uppercase", "error", fmt.Sprint(err != nil)}
+		mw.requestCount.With(lvs...).Add(1)
+		mw.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
+	}(time.Now())
+
+	output, err = mw.next.Uppercase(s)
+	return
+}
+
+func (mw Middleware) Count(s string) (n int) {
+	defer func(begin time.Time) {
+		lvs := []string{"method", "count", "error", "false"}
+		mw.requestCount.With(lvs...).Add(1)
+		mw.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
+		mw.countResult.Observe(float64(n))
+	}(time.Now())
+
+	n = mw.next.Count(s)
+	return
+}

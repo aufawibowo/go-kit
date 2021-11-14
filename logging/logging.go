1ed6e69a6386c28255bc0651f45b54ec0f964bbe
aufawibowo <muhammad.aufa@fazzfinancial.com>
2021-11-14 16:13:16 +0700

    implement method

--- logging/logging.go
+++ logging/logging.go
@@ -3,9 +3,39 @@ package logging
 import (
 	"github.com/aufawibowo/go-kit/service"
 	"github.com/go-kit/kit/log"
+	"time"
 )
 
 type loggingMiddleware struct {
 	logger log.Logger
 	next   service.StringService
+}
+
+func (mw loggingMiddleware) Uppercase(s string) (output string, err error) {
+	defer func(begin time.Time) {
+		_ = mw.logger.Log(
+			"method", "uppercase",
+			"input", s,
+			"output", output,
+			"err", err,
+			"took", time.Since(begin),
+		)
+	}(time.Now())
+
+	output, err = mw.next.Uppercase(s)
+	return
+}
+
+func (mw loggingMiddleware) Count(s string) (n int) {
+	defer func(begin time.Time) {
+		_ = mw.logger.Log(
+			"method", "count",
+			"input", s,
+			"n", n,
+			"took", time.Since(begin),
+		)
+	}(time.Now())
+
+	n = mw.next.Count(s)
+	return
 }
